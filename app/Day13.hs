e3cf3cbfce4b720026444e87c31f89903aaa7dac
tichymi5 <michal.tichy.y@email.cz>
2023-08-01 22:02:49 +0200

    Add day 13 part two

--- app/Day13.hs
+++ app/Day13.hs
@@ -2,11 +2,12 @@ module Day13 where
 
 import Common (Problem (Problem), runProblem)
 import qualified Control.Monad
+import Data.List (sort, sortBy)
 import Data.List.Split (splitOn)
 import Data.Void (Void)
 import System.FilePath ((</>))
 import Tests ()
-import Text.Megaparsec (Parsec, between, many, parseMaybe, sepBy, (<|>))
+import Text.Megaparsec (Parsec, between, many, parseMaybe, sepBy, (<|>), MonadParsec (eof), sepEndBy)
 import Text.Megaparsec.Char (char, eol)
 import Text.Megaparsec.Char.Lexer (decimal, lexeme)
 
@@ -15,7 +16,7 @@ type CharParser a = Parsec Void String a
 data Cell
   = Num Integer
   | List List
-  deriving (Show)
+  deriving (Show, Eq)
 
 data TriBool = T | F | N
   deriving (Eq)
@@ -24,9 +25,25 @@ type List = [Cell]
 
 day :: IO ()
 day = do
-  let easy = Problem parseDay solveEasy show
+  let easy = Problem parseEasy solveEasy show
+  let hard = Problem parseHard solveHard show
   let inputFilename = "app" </> "inputs" </> "13.txt"
-  runProblem inputFilename easy
+  runProblem inputFilename hard
+
+solveHard :: [List] -> Maybe Int
+solveHard ins = Just $ snd (head (filter (\(l, i) -> l == firstDivider) wIdx)) * snd (head (filter (\(l, i) -> l == secondDivider) wIdx))
+  where
+    firstDivider = [List [List [Num 6]]]
+    secondDivider = [List [List [Num 2]]]
+    inputs = firstDivider : secondDivider : ins
+    sorted = sortBy comparison inputs
+    wIdx = zip sorted [1 ..]
+
+comparison :: List -> List -> Ordering
+comparison fsts snds = case isRightOrder fsts snds of
+  T -> LT
+  F -> GT
+  N -> EQ
 
 solveEasy :: [(List, List)] -> Maybe Integer
 solveEasy input = Just $ sum $ map snd $ filter (\(res, idx) -> res == T) $ zip results [1 ..]
@@ -47,8 +64,11 @@ isRightOrder [] [] = N
 isRightOrder [] rs = T
 isRightOrder ls [] = F
 
-parseDay :: String -> Maybe [(List, List)]
-parseDay = parseMaybe (sepBy parsePair eol)
+parseHard :: String -> Maybe [List]
+parseHard = parseMaybe (sepEndBy parseList (many eol) <* eof)
+
+parseEasy :: String -> Maybe [(List, List)]
+parseEasy = parseMaybe (sepBy parsePair eol)
 
 parsePair :: CharParser (List, List)
 parsePair = do
@@ -59,10 +79,7 @@ parsePair = do
   return (first, second)
 
 parseCell :: CharParser Cell
-parseCell =
-  lexeme
-    (Control.Monad.void (many (char ' ')))
-    $ fmap Num decimal <|> fmap List (between (char '[') (char ']') parseList)
+parseCell = lexeme (Control.Monad.void (many (char ' '))) $ fmap Num decimal <|> fmap List parseList
 
 parseList :: CharParser [Cell]
-parseList = sepBy parseCell (char ',')
+parseList = between (char '[') (char ']') $ sepBy parseCell (char ',')
